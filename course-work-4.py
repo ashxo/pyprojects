b9cee8decb7a25beacdbf2c25dda3f2304ae6794
Oleksandra Pavlova <oleksandra.v.pavlova@synapse.com>
2017-07-06 12:48:21 +0300

    course work 4 updated

--- course-work-4.py
+++ course-work-4.py
@@ -1,56 +1,30 @@
 from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
-import time
 
 
 def test_google():
-
-    driver = webdriver.Chrome();
-    submenuCheck(driver)
-    driver.quit()
-
-
-def submenuCheck(driver):
-
-    wait = WebDriverWait(driver, 3)
-    submenuWait = WebDriverWait(driver, 0.5)
-    headerPage = 'h1'
-    driver.get("http://localhost/litecart/public_html/admin/login.php")
-    driver.find_element_by_name("username").send_keys("admin")
-    driver.find_element_by_name("password").send_keys("admin")
+    driver = webdriver.Google()
+    driver.get("http://localhost/litecart/admin/")
+    driver.find_element_by_name('username').send_keys('admin')
+    driver.find_element_by_name('password').send_keys('admin')
     driver.find_element_by_css_selector('button.btn.btn-default').click()
-    wait.until(ec.title_is("My Store"))
 
-    cssMenu = 'li[id="app-"]'
-    presenceMenu = wait.until(ec.presence_of_element_located((By.CSS_SELECTOR, cssMenu)))
-    menu = driver.find_elements_by_css_selector(cssMenu)
+    driver.implicitly_wait(3)
 
-    for index in range(1, len(menu) + 1):
-        menuItem = cssMenu + ':nth-child(' + str(index) + ')'
-        wait.until(ec.element_to_be_clickable((By.CSS_SELECTOR, menuItem)))
-        driver.find_element_by_css_selector(menuItem).click()
-        assert (wait.until
-                (ec.visibility_of_element_located((By.CSS_SELECTOR, headerPage)))), "Element h1 not found"
+    links = len(driver.find_elements_by_css_selector("ul#box-apps-menu li#app-"))
 
-        #submenu
-        cssSubMenu = 'li[id^="doc-"]'
-        try:
-            subMenuPresence = submenuWait.until(
-                ec.presence_of_all_elements_located((By.CSS_SELECTOR, cssSubMenu)))
-        except :
-            subMenuPresence = False
-        if subMenuPresence:
-            subMenu = driver.find_elements_by_css_selector(cssSubMenu)
-            for index in range(1, len(subMenu) + 1):
-                subMenuItem = cssSubMenu + ':nth-child(' + str(index) + ')'
-                wait.until(ec.element_to_be_clickable((By.CSS_SELECTOR, subMenuItem)))
-                driver.find_element_by_css_selector(subMenuItem).click()
-                assert (wait.until
-                        (ec.visibility_of_element_located((By.CSS_SELECTOR, headerPage)))), "Element h1 not found"
-
-    # Logout:
-    logoutLocator = 'i[class="fa fa-sign-out fa-lg"]'
-    wait.until(ec.element_to_be_clickable((By.CSS_SELECTOR, logoutLocator)))
-    driver.find_element_by_css_selector(logoutLocator).click()
+    for l in range(links):
+        driver.find_elements_by_css_selector("ul#box-apps-menu li#app-")[l].click()
+        WebDriverWait(driver, 1).until(
+            EC.presence_of_element_located((By.TAG_NAME, "h1")))
+        if len(driver.find_elements_by_css_selector("ul#box-apps-menu li.selected ul li")) != 0:
+            sublinks = len(driver.find_elements_by_css_selector("ul#box-apps-menu "
+                                                                "li.selected ul li"))
+            for s in range(sublinks):
+                driver.find_elements_by_css_selector("ul#box-apps-menu "
+                                                     "li.selected ul li")[s].click()
+                WebDriverWait(driver, 1).until(
+                    EC.presence_of_element_located((By.TAG_NAME, "h1")))
+    driver.close()
